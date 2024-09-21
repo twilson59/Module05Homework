e8310aa8a9827427bc31bbe57ace99a8f585ca63
Travis Wilson <TWilson59@ivytech.edu>
2024-09-21 18:14:05 -0500

    Remove tracking

--- mod5project/mod5app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from mod5app.models import Manufacturer, Appliance
-
-
-
-# Register your models here.
-admin.site.register(Manufacturer)
-admin.site.register(Appliance)
