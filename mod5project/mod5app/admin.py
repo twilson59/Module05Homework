ab1bc3ff15cb6eb559619d1b4b7db7aa99b68101
Travis Wilson <TWilson59@ivytech.edu>
2024-09-21 18:17:20 -0500

    Recommit

---
+++ mod5project/mod5app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from mod5app.models import Manufacturer, Appliance
+
+
+
+# Register your models here.
+admin.site.register(Manufacturer)
+admin.site.register(Appliance)
